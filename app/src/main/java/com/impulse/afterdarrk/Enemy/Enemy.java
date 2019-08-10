52b43b006fc28179c3f909dc75778a416c4731e9
JamesDalboth <James.Dalboth@gmail.com>
2019-08-10 16:43:50 +0100

    Add Enemy action list above them

--- app/src/main/java/com/impulse/afterdarrk/Enemy/Enemy.java
+++ app/src/main/java/com/impulse/afterdarrk/Enemy/Enemy.java
@@ -3,6 +3,7 @@ package com.impulse.afterdarrk.Enemy;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.media.Image;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,6 +16,8 @@ import com.impulse.afterdarrk.Player;
 import com.impulse.afterdarrk.Utils.CartesianCoords;
 import com.impulse.afterdarrk.Utils.PolarCoords;
 
+import java.util.List;
+
 public abstract class Enemy extends DisplayObj {
     // Initial Distance for enemies to spawn at
     static int INIT_DISTANCE = 1000;
@@ -23,6 +26,7 @@ public abstract class Enemy extends DisplayObj {
     public static int NUM_ENEMIES = 0;
     // Radius of enemy to draw
     public static int RADIUS = 100;
+    public static int ACTION_WIDTH = 50;
 
     protected Player player;
     private EnemyType type;
@@ -85,16 +89,41 @@ public abstract class Enemy extends DisplayObj {
 
     public void draw(Canvas canvas) {
         Paint paint = new Paint();
+
         if (player.getEnemy() == this) {
             paint.setColor(Color.BLUE);
         } else {
             paint.setColor(Color.BLACK);
         }
-        canvas.drawCircle(Math.round(getPosition().getX()) ,
+
+        canvas.drawCircle(Math.round(getPosition().getX()),
                                      Math.round(getPosition().getY() - (RADIUS/2)), RADIUS, paint);
+
+        drawActions(canvas);
     }
 
-    public CartesianCoords getPosition() {
+    private void drawActions(Canvas canvas) {
+        Paint paint = new Paint();
+
+        paint.setColor(Color.BLACK);
+
+        List<ActionType> remaining = actions.remaining();
+
+        double width = remaining.size() * ACTION_WIDTH + (remaining.size() - 1) * ACTION_WIDTH/2;
+
+        double left = getPosition().getX() - width/2;
+
+        int rectTop = (int) Math.floor(getPosition().getY()) - RADIUS - ACTION_WIDTH * 2;
+
+        for (int i = 0; i < remaining.size(); i++) {
+            int rectLeft = (int) Math.round(left + (i * ACTION_WIDTH * 3/2));
+
+            Rect actionRect = new Rect(rectLeft, rectTop, rectLeft + ACTION_WIDTH, rectTop + ACTION_WIDTH);
+            canvas.drawRect(actionRect, paint);
+        }
+    }
+
+    private CartesianCoords getPosition() {
         return polar.toCatesian().addOff(new CartesianCoords(Main.width/2, Main.height/2));
     }
 
