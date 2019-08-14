7bcacf4fc673319fe1156ede9719b851d4702193
theriverneil <34864441+theriverneil@users.noreply.github.com>
2019-08-15 00:20:00 +0700

    Main menu working
    * Make sizing and position of elements relative
    
    * Main menu working
    
    With initial menu art

---
+++ app/src/main/java/com/impulse/afterdarrk/MainMenu.java
@@ -0,0 +1,28 @@
+package com.impulse.afterdarrk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        configureStartButton();
+
+    }
+
+    private void configureStartButton() {
+        Button startButton = (Button) findViewById(R.id.button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, Main.class));
+            }
+        });
+    }
+}
